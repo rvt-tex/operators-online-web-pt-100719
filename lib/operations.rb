228c70874e6737cf36c9df09a5fc61cdd4a809f0
Ruel Tingling <rvtingling@yahoo.com>
2019-10-09 19:46:52 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,7 @@
 def unsafe?(speed)
-
+  if speed is <40 || >60 do
+    puts "Going 30 mph on the freeway would be unsafe, as would going 95 mph."
+  else puts "Going 50 miles per hour, however, would return false as that's within the "safe" range."
 end
 
 
